304ee0dcf5de01e9840ad4ff65267be915684998
Kintu Declan <d-kintu@outlook.com>
2026-02-04 23:34:28 -0500

    Frontend

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-
+  base: '/theGarage-services.github.io/'
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
