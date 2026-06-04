85b550a5eb485078491483c68f337b560c95e8e5
Kintu Declan <d-kintu@outlook.com>
2026-06-04 18:56:36 +0100

    Cleaner Version

--- current/src/components/auth/signup-steps/index.ts
+++
@@ -1,5 +0,0 @@
-export { AccountStep } from './AccountStep';
-export { ProfileStep } from './ProfileStep';
-export { ResumeStep } from './ResumeStep';
-export { InstitutionStepWrapper } from './InstitutionStepWrapper';
-export { PreferencesSetup } from './PreferencesStep';export type { IndividualJobPreferences } from './PreferencesStep';
--- current/src/components/recruiter/views/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * View components exports for Job Management
- */
-export { JobDetailView } from './JobDetailView';
-export { EditJobView } from './EditJobView';
-export { CandidatesView } from './CandidatesView';
-export { ListView } from './ListView';
